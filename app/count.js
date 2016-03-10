58dbf8001b784349db45150dc0f1df7655fb0bc1
NHB348 <nhb348@zebra.com>
2016-03-10 18:40:35 +0530

    test

--- app/count.js
+++ app/count.js
@@ -2,6 +2,22 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var c,i = start;
+	function test()
+	 {
+	   if(start < end + 1)
+	    {console.log(starttoString());}
+		start = start + 1;
+	}
 
+        
+     c = setInterval(test, 100);
+        
+    
+    function cancel()
+   {
+       clearInterval(c);
+    }
+    
   }
 };
