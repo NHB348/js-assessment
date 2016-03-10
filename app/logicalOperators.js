3869ce3d99fff9ef8faa13a22c2459f4610a5cf0
NHB348 <nhb348@zebra.com>
2016-03-10 18:42:22 +0530

    test

--- app/logicalOperators.js
+++ app/logicalOperators.js
@@ -2,10 +2,18 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.logicalOperatorsAnswers = {
   or : function(a, b) {
-
+     if(a == true || b == true)
+	  return true;
+	 else 
+	  return false;
+	 
   },
 
   and : function(a, b) {
-
+         if(Boolean(a) == true  && Boolean(b) == true)
+             return true;
+	     else 
+		 return false;
+	    
   }
 };
