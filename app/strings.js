950d96aa758f823935e5746c7931ff4d7fa8379c
NHB348 <nhb348@zebra.com>
2016-03-10 18:43:27 +0530

    test

--- app/strings.js
+++ app/strings.js
@@ -2,12 +2,35 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
+     var result = [];
+    
+        var i = null;
+        var length = str.length;
+        var previous = null;
+        for (i = 0; i < length; i += amount)
+        {
+            var current = str.charAt(i);
+    
+            if (current !== previous)
+            {
+                result.push(current);
+            }
+    
+            previous = current;
+        }
+    
+        return result.join("");
 
   },
   wordWrap: function(str, cols) {
 
   },
   reverseString: function(str) {
+            
+   return   str.split('').reverse().join('');
+      
+    
 
   }
 };
+
