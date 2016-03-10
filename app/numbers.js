f7a6432a98a59ef51b052c4b700d33c05ce052af
NHB348 <nhb348@zebra.com>
2016-03-10 18:42:55 +0530

    test

--- app/numbers.js
+++ app/numbers.js
@@ -6,14 +6,21 @@ exports.numbersAnswers = {
   },
 
   base10: function(str) {
-
+     return  parseInt( str.split('').join(''), 2 );
   },
 
   convertToBinary: function(num) {
-
+     var oct = parseInt(num, 8);
+	 return (oct >>> 0).toString(2);
   },
 
   multiply: function(a, b) {
+      var commonMultiplier = 1000000;
+
+    a *= commonMultiplier;
+    b *= commonMultiplier;
+
+    return (a * b) / (commonMultiplier * commonMultiplier);
 
   }
 };
